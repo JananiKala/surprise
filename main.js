e49e505a0fccc64979f127e0a1ef98c8ba730c0c
JananiKala <jananikumaresan2004.r@gmail.com>
2025-06-14 07:15:08 +0530

    changes

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function fetchdetails(){
+
+    let name=document.getElementById("name").value;
+let number=document.getElementById("number").value;
+document.getElementById("displayName").textContent = name;
+      document.getElementById("displayNumber").textContent = number;
+
+}
